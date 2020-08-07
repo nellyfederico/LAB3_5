9c035c5bc08b4aa24e30f35a85822a9abf0dc337
nellyfederico <nallelyfedericoc@gmail.com>
2020-08-07 16:50:34 -0500

    LAB3_5

---
+++ LAB3_5SpamChecker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LAB3_5SpamChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "pills";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("This email is spam and it has been sent to the Spam Folder!");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("This email is not spam.");
+            }
+        }
+    }
+}
